27fd8d10b74a429ce8bfbae1d098fc1d707318a5
Anand-S23 <anandsroopsingh23@gmail.com>
2024-07-19 22:27:16 -0400

    Camera Zoom

--- scenes/Dynamic_Camera.gd
+++ scenes/Dynamic_Camera.gd
@@ -1,9 +1,13 @@
 extends Camera2D
 
 const MAX_DISTANCE = 48
+const MAX_ZOOM = 10.0
+const MIN_ZOOM = 4.0
+const SCROLL_SPEED = 0.4
 
 var target_distace := 0
 var center_pos := position
+var zoom_pos := 4.0
 
 func _process(delta):
 	var direction := center_pos.direction_to(get_local_mouse_position())
@@ -14,7 +18,16 @@ func _process(delta):
 		center_pos + Vector2(MAX_DISTANCE, MAX_DISTANCE)
 	)
 	position = target_pos
+	
+	zoom_pos = clamp(zoom_pos, MIN_ZOOM, MAX_ZOOM)
+	zoom = Vector2(zoom_pos, zoom_pos)
 
 func _input(event):
 	if event is InputEventMouseMotion:
 		target_distace = center_pos.distance_to(get_local_mouse_position()) / 3
+	elif event is InputEventMouseButton:
+		if event.is_pressed():
+			if event.button_index == MOUSE_BUTTON_WHEEL_UP:
+				zoom_pos += SCROLL_SPEED
+			elif event.button_index == MOUSE_BUTTON_WHEEL_DOWN:
+				zoom_pos -= SCROLL_SPEED
